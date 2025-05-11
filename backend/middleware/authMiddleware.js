cbf2f1c09d8b7d38efc41039a0fddac01b7ce670
José Balbuena <45837292+JoseBalbuena181096@users.noreply.github.com>
2025-05-10 22:53:47 -0600

    verificar un JWT en el servidor

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -1,12 +1,23 @@
+import jwt from 'jsonwebtoken';
+
 const authMiddleware = async(req, res, next) =>{
     if(req.headers.authorization && req.headers.authorization.startsWith('Bearer')){
-        console.log('JWT encontrado');
-        
+        // console.log('JWT encontrado');
+        try {
+            const token = req.headers.authorization.split(' ')[1];
+            const decoded = jwt.verify(token, process.env.JWT_SECRET);
+            console.log(decoded);
+        } catch (error) {
+            const error_ = new Error(error.message);
+            return res.status(403).json({
+                msg: error_.message
+            });
+        }
     }
     else{
         // console.log('JWT no encontrado');
         const error = new Error('Token no encontrado o invalido');
-        return res.status(401).json({
+        return res.status(403).json({
             msg: error.message
         });
     }
