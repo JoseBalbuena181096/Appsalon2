188ee79d3ac363763055f5c3707a174864f42519
José Balbuena <45837292+JoseBalbuena181096@users.noreply.github.com>
2025-05-10 22:21:28 -0600

    Protegiendo rutas con el guard de navegacion

--- frontend/src/views/appointments/AppoitmentsLayout.vue
+++ frontend/src/views/appointments/AppoitmentsLayout.vue
@@ -14,11 +14,12 @@
             <nav 
                 class="flex gap-2 items-center justify-center"
             >
-                <button
+                <RouterLink
                     class="p-3 text-gray-200 uppercase text-xs font-black rounded-lg"
+                    :to="{ name: 'my-appointments'}"
                 >
                     Mis Citas
-                </button>
+                </RouterLink>
                 <RouterLink
                     class="p-3 text-gray-200 uppercase text-xs font-black rounded-lg bg-blue-600 hover:bg-blue-700"
                     :to="{ name: 'new-appointment'}"
---
+++ frontend/src/views/appointments/MyAppointmentsView.vue
@@ -0,0 +1,10 @@
+<script setup>
+
+</script>
+
+<template>
+    <h2 class="text-4xl font-extrabold text-white">Mis citas</h2>
+    <p class="text-white text-lg">
+        A continuación podrás tus próximas citas
+    </p>
+</template>
