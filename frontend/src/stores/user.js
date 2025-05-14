36f4c1440b2afc6cbbf046456829e63874fa3d4f
José Balbuena <45837292+JoseBalbuena181096@users.noreply.github.com>
2025-05-14 17:02:55 -0600

    Obtener citas del usuario en VUE

--- frontend/src/stores/user.js
+++ frontend/src/stores/user.js
@@ -2,19 +2,27 @@ import { defineStore } from 'pinia';
 import {ref, onMounted, computed} from 'vue';
 import AuthAPI from '../api/AuthAPI'; 
 import { useRouter } from 'vue-router';
+import AppointmentAPI from '../api/AppointmentAPI';
 
 export const useUserStore = defineStore('user', () => {
     const router = useRouter();
     const user = ref({});
+    const userAppointments = ref([]);
 
     onMounted(async () => {
         try {
             const {data} = await AuthAPI.auth();
             user.value = data;
+            await getUserAppointments();
         } catch (error) {
             console.error(`Error: ${error.message}`);
         }
     });
+
+    async function getUserAppointments(){
+        const {data} = await AppointmentAPI.getUserAppointments(user.value._id);
+        userAppointments.value = data;
+    }
     
     const getUserName = computed(() => {
         return user.value?.name ? user.value?.name : '';
@@ -25,9 +33,15 @@ export const useUserStore = defineStore('user', () => {
         router.push({name: 'login'});
     }
 
+    const noAppointments = computed(() => {
+        return userAppointments.value.length === 0;
+    });
+    
     return{
         user,
         getUserName,
-        logout
+        logout,
+        userAppointments,
+        noAppointments,
     }
 });
