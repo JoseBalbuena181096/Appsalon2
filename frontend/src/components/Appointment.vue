23302c4705ac8109a1d415c35b27411e6d74aaef
José Balbuena <45837292+JoseBalbuena181096@users.noreply.github.com>
2025-05-14 18:18:08 -0600

    Detalles de la cita pt2

--- frontend/src/components/Appointment.vue
+++ frontend/src/components/Appointment.vue
@@ -1,5 +1,6 @@
 <script setup>
     import {displayDate} from '../helpers/date';
+    import {formatCurrency} from '../helpers';
 
     defineProps({
         appointment: {
@@ -17,7 +18,35 @@
             Fecha: <span class="font-light">{{displayDate(appointment.date)}}</span>
             Hora: <span class="font-light">{{appointment.time}} Horas.</span>
         </p>
-
+        <p class="text-lg font-black">
+            Servicios solicitados en la cita
+        </p>
+        <div v-for="service in appointment.services">
+            <p>
+                {{service.name}}
+            </p>
+            <p class="text-blue-500 font-black text-2xl">
+                {{formatCurrency(service.price)}}
+            </p>
+        </div>
+        <p class="text-right font-black text-2xl">
+            Total a pagar: 
+            <span class="text-blue-600">{{formatCurrency(appointment.totalAmount)}}</span>
+        </p>
+        <div class="flex gap-2 items-center">
+            <button 
+                type="button" 
+                class="bg-slate-600 hover:bg-slate-700 rounded-lg text-white p-3 text-sm font-black flex-1 md:flex-none"
+            >
+                Editar cita
+            </button>
+            <button 
+                type="button" 
+                class="bg-red-600 hover:bg-red-700 rounded-lg text-white p-3 text-sm font-black flex-1 md:flex-none"
+            >
+                Cancelar cita
+            </button>
+        </div>
     </div>
 </template>
 
